76dc205cea7d87526a7c90b0557f7b842c2f8a77
jamesmhayne <44489714+jamesmhayne@users.noreply.github.com>
2018-11-15 23:19:58 +1000

    added php and switched to bootstrap

---
+++ html/header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css" />
+    <title>Tech Mate</title>
+  </head>
+  <body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+    <div class="row">
+      <div class="col-sm-12">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            <img src="images/header-logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Tech Mate
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="business.php">Business Solutions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="home.php">Home Solutions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="partners.php">Partners</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+          </div>
+        </nav>
+      </div>
+    </div>
---
+++ html/index.php
@@ -0,0 +1,38 @@
+<?php include 'header.php';?>
+    <div class="container">
+      <div class="card-deck m-3">
+        <div class="card m-10 shadow-sm">
+          <div class="card-body">
+            <h3 class="card-title">WELCOME TO TECH MATE<br>
+              <div class="card-sub-heading">Your mate when there's tech trouble</div>
+            </h3>
+            <p class="card-text">
+              Here at Tech Mate our aim is to provide simple and budget solutions to technological problems.
+            </p>
+            <h5>Business Solutions</h5>
+            <p class="card-text">
+              For businesses the services we provide range of services that can help a business move into or expand thier online presence.
+              <ul>
+                <li>In house graphic designer</li>
+                <li>Web page</li>
+                <li>Online collaboration tools</li>
+                <li>Social media</li>
+                <li>Advertising</li>
+              </ul>
+            </p>
+            <h5>Home Solutions</h5>
+            <p class="card-text">
+              At home we can provide help with all the tech problems that people face at home.
+              <ul>
+                <li>Family communication</li>
+                <li>Get Netflix on the TV</li>
+                <li>Online privacy</li>
+                <li>Home networks</li>
+                <li>Home movie and photo backup</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php include 'footer.php';?>
