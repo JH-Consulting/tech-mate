915ca9d4fa6bb4c7c966f61a3d8d5e0d841025a3
jamesmhayne <44489714+jamesmhayne@users.noreply.github.com>
2018-11-17 18:27:11 +1000

    more updates

--- html/index.php
+++ html/index.php
@@ -1,41 +1,52 @@
 <?php include 'header.php';?>
-    <div class="container">
-      <div class="card-deck m-3">
-        <div class="card m-10 shadow-sm">
-          <div class="card-body">
-            <h3 class="card-heading">WELCOME TO TECH MATE
-              <br>
-              <div class="card-sub-heading">Your mate when there's tech trouble</div>
-            </h3>
-            <p class="card-text">
-              Here at Tech Mate our aim is to provide simple and budget solutions to technological problems.
-            </p>
-            <h5 class="card-heading"><i class="fas fa-building"></i>&nbsp;Business Solutions</h5>
-            <p class="card-text">
-              For businesses the services we provide range of services that can help a business move into or expand thier online presence.
-              <ul class="card-text">
-                <li>In house graphic designer</li>
-                <li>Web page</li>
-                <li>Online collaboration tools</li>
-                <li>Social media</li>
-                <li>Advertising</li>
-              </ul>
-              <a href="home.php"class="btn btn-primary btn-sm">More information</a>
-            </p>
-            <h5 class="card-heading"><i class="fas fa-home"></i>&nbsp;Home Solutions</h5>
-            <p class="card-text">
-              At home we can provide help with all the tech problems that people face at home.
-              <ul class="card-text">
-                <li>Family communication</li>
-                <li>Get Netflix on the TV</li>
-                <li>Online privacy</li>
-                <li>Home networks</li>
-                <li>Home movie and photo backup</li>
-              </ul>
-              <a href="business.php"class="btn btn-primary btn-sm">More information</a>
-            </p>
-          </div>
-        </div>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card m-4 shadow-sm">
+      <div class="card-body">
+        <h3 class="card-heading">WELCOME TO TECH MATE
+          <br>
+          <div class="card-sub-heading">Your mate when there's tech trouble</div>
+        </h3>
+        <p class="card-text">
+          Here at Tech Mate our aim is to provide simple and budget solutions to technological problems.
+        </p>
       </div>
     </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card m-4 shadow-sm">
+      <div class="card-body">
+        <h5 class="card-heading"><i class="fas fa-building"></i>&nbsp;Business Solutions</h5>
+        <p class="card-text">
+          For businesses the services we provide range of services that can help a business move into or expand thier online presence.
+          <ul class="card-text">
+            <li>In house graphic designer</li>
+            <li>Web page</li>
+            <li>Online collaboration tools</li>
+            <li>Social media</li>
+            <li>Advertising</li>
+          </ul>
+          <a href="home.php"class="btn btn-primary btn-sm">More information</a>
+        </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card m-4 shadow-sm">
+      <div class="card-body">
+        <h5 class="card-heading"><i class="fas fa-home"></i>&nbsp;Home Solutions</h5>
+        <p class="card-text">
+          At home we can provide help with all the tech problems that people face at home.
+          <ul class="card-text">
+            <li>Family communication</li>
+            <li>Get Netflix on the TV</li>
+            <li>Online privacy</li>
+            <li>Home networks</li>
+            <li>Home movie and photo backup</li>
+          </ul>
+          <a href="business.php"class="btn btn-primary btn-sm">More information</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
 <?php include 'footer.php';?>
