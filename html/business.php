80ab67ddd7fdb6546b76c4c03015391cd2323bbd
jamesmhayne <44489714+jamesmhayne@users.noreply.github.com>
2018-11-18 23:15:21 +1000

    added first modal

--- html/business.php
+++ html/business.php
@@ -26,7 +26,9 @@
             <li>Advertising and marketing materials</li>
             <li>Image and branding consultation</li>
           </ul>
-          <a href="#"class="btn btn-primary btn-sm">More information</a>
+          <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#inHouseGraphicDesignerModal">
+            More information
+          </button>
         </div>
       </div>
     </div>
@@ -80,4 +82,25 @@
     </div>
   </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="inHouseGraphicDesignerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">In house graphic designer</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Examples of other websites.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php include 'footer.php';?>
