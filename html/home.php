21a2f48929225fbecaa56fe183e4c4c2a183d1d3
jamesmhayne <44489714+jamesmhayne@users.noreply.github.com>
2018-11-25 11:35:57 +1000

    updated home page

--- html/home.php
+++ html/home.php
@@ -1,5 +1,108 @@
 <?php include 'header.php';?>
-<div class="row">
+<div class="row secondary-page">
+  <div class="col-sm-7">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="page-description-text">
+          Tech Mate strives to make the home solutions we provide intuative to use, easy to maintian and on a budget everyone can afford.
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="page-content-text">
+          The solutions we provide range from simple and easy to use family communication to utilising your TV to view photos, videos and movies.
+          We also want our clients to have confidence and peace of mind when at home.  For this we provide consultion on online privacy and security for using the internt,
+          home networks and secure backup strategies for documents and photos and home movies.
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-1"></div>
+  <div class="col-sm-4">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-comments"></i>&nbsp;FAMILY COMMUNICATION
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Cheaply and easily stay in touch</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              Setup a simple and easy to use family communication.  Utilising everything from group chats, to newsletters and video phone.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-tv"></i>&nbsp;SHOW ON TV
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Use your TV as a display</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              With some simple technology, an HDMI TV and a wifi network, setup you TV to show everything from photos and home movies to Netflix.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-shield-alt"></i>&nbsp;ONLINE PRIVACY
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Safe and secure internet usage</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              Setup some simple and easy techniques recommended by industry experts for maximising security and confidence online.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-network-wired"></i>&nbsp;NETWORK SUPPORT
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Keep everything at home running smoothly</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              Ensure your home network is utilising it's full potential for students, home business or general usage.
+              <br><br>
+                Extend wifi range<br>
+                Install netwrok printers<br>
+                Security<br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-toolbox"></i>&nbsp;BACKUP
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Document, photo and other important data backed up and safe</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              Implement some simple and secure backup strategies to ensure that computer issues no longer cause loss of import data.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- <div class="row">
   <div class="col-sm-6">
     <div class="card m-4 shadow-sm">
       <div class="card-body">
@@ -48,7 +151,7 @@
       <div class="card-body">
         <h4 class="card-heading">Online privacy</h4>
         <div class="card-text">
-          In today's climate it pays to have a level on online privacy and security.
+          In today's climate it pays to have a level of online privacy and security.
           <br><br>
           <ul class="card-text">
             <li>VPN - Virtual Private Network</li>
@@ -95,5 +198,5 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 <?php include 'footer.php';?>
