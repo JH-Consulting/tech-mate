44bdd7ce515a467137d96d825f70034cecb89aed
jamesmhayne <44489714+jamesmhayne@users.noreply.github.com>
2018-11-25 12:32:40 +1000

    updated business page

--- html/business.php
+++ html/business.php
@@ -1,106 +1,84 @@
 <?php include 'header.php';?>
-<div class="row">
-  <div class="col-sm-6">
-    <div class="card m-4 shadow-sm">
-      <div class="card-body">
-        <h3 class="card-heading">BUSINESS SOLUTIONS
-          <br>
-          <div class="card-sub-heading">Helping businesses reach thier potential</div>
-        </h3>
-        <p class="card-text">
+<div class="row secondary-page">
+  <div class="col-sm-7">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="page-description-text">
           Tech Mate wants to help and enable business to make use of the tools and technology available to them to reach as many customers as they can.
-        </p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="col-sm-3">
-    <div class="card m-4 shadow-sm">
-      <div class="card-body">
-        <h4 class="card-heading">In house graphic designer</h4>
-        <div class="card-text">
-          Our in house graphic designer has over 10 years in the graphoc design, branding and advertising space.  He can help you to project the image you wish your customers to see.
-          <br><br>
-          <ul class="card-text">
-            <li>Web pages</li>
-            <li>Logos</li>
-            <li>Advertising and marketing materials</li>
-            <li>Image and branding consultation</li>
-          </ul>
-          <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#inHouseGraphicDesignerModal">
-            More information
-          </button>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="page-content-text">
+          From branding to online presence consulting to in store advertising solutions Tech Mate can help your business reach more people with
+          a focus on showing what you can do for them, how they can reach you and how you will appreciate your business.
         </div>
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card m-4 shadow-sm">
-      <div class="card-body">
-        <h4 class="card-heading">Web pages</h4>
-        <div class="card-text">
-          We create simple and effective web pages created in consultation with yourself and our graphic designers.
-          <br><br>
-          <ul class="card-text">
-            <li>Web pages</li>
-            <li>Domain name registration</li>
-            <li>Cheap and reliable web hosting</li>
-          </ul>
-          <a href="#"class="btn btn-primary btn-sm">More information</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="row">
+  <div class="col-sm-1"></div>
   <div class="col-sm-4">
-    <div class="card m-4 shadow-sm">
-      <div class="card-body">
-        <h4 class="card-heading">Social media</h4>
-        <div class="card-text">
-          Social media can be tough to keep up with.  Tech Mate can provide help designing the right social media strategy for your business.
-          <br><br>
-          <a href="#"class="btn btn-primary btn-sm">More information</a>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-crop"></i>&nbsp;GRAPHIC DESIGN
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">In house graphic designer</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              Our in house graphic designer has over 10 years in the graphoc design, branding and advertising space.  He can help you to project the image you wish your customers to see.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card m-4 shadow-sm">
-      <div class="card-body">
-        <h4 class="card-heading">Advertising</h4>
-        <div class="card-text">
-          Using simple but effective technology Tech Mate can help setup instore advertising solutions.
-          <br><br>
-          <ul class="card-text">
-            <li>Advertisements videos</li>
-            <li>Slideshows</li>
-            <li>Presentation tools</li>
-          </ul>
-          <a href="#"class="btn btn-primary btn-sm">More information</a>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-globe-asia"></i>&nbsp;WEB PAGE
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Project your business onto the web</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              We create simple and effective web pages created in consultation with yourself and our graphic designers.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="far fa-user-circle"></i>&nbsp;SOCIAL NETWORK
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Maximise social media</div>
+            </h3>
+            <p class="card-text blue-card-text">
+              Social media provides some excellent tools for reaching customers.  Learn how to make use of them in an effective, yet simple and maintainable way.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card m-12 blue-card hint-card">
+          <div class="card-body blue-card-body">
+            <h3 class="card-heading blue-card-heading"><i class="fas fa-ad"></i>&nbsp;ADVERTISING
+              <br>
+              <div class="card-sub-heading blue-card-sub-heading">Instore advertising solutions</div>
+            </h3>
+            <p class="card-text blue-card-text">
+                Using simple but effective technology instore advertising solutions can be provided on a budget.
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-
-<!-- Modal -->
-<div class="modal fade" id="inHouseGraphicDesignerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">In house graphic designer</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Examples of other websites.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 <?php include 'footer.php';?>
